5786e0f54115b4b9420afeaf6cc2c1ab2d52eec8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-18 14:41:26 -0500

    added code to secure game pages

--- server/app.js
+++ server/app.js
@@ -60,6 +60,13 @@ app.use(passport.session());
 app.use('/', index);
 app.use('/games', games);
 
+// Passport User Configuration
+let UserModel = require('./models/users');
+let User = UserModel.User; // alias for the User Model - User object
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Handle 404 Errors
   app.use(function(req, res) {
       res.status(400);
@@ -76,25 +83,5 @@ app.use('/games', games);
         error: error
       });
   });
-
-/*
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) =>{
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
 
 module.exports = app;
